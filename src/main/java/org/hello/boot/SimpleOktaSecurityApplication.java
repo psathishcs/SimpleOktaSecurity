7a606ba8a5b8d401cdc92aa2cad4cead003b6284
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2018-01-04 18:20:43 -0600

    adding Okta OAuth

--- src/main/java/org/hello/boot/SimpleOktaSecurityApplication.java
+++ src/main/java/org/hello/boot/SimpleOktaSecurityApplication.java
@@ -2,8 +2,10 @@ package org.hello.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SimpleOktaSecurityApplication {
 	public static void main(String args[]) throws Exception {
 		SpringApplication.run(SimpleOktaSecurityApplication.class, args);
--- src/main/java/org/hello/boot/controller/CountryController.java
+++ src/main/java/org/hello/boot/controller/CountryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ public class CountryController {
 	private CountryRepository countryRepository;
 
 	@RequestMapping(value="/country", method = RequestMethod.GET,  produces="application/json")
+	@Secured("ROLE_ADMIN")
 	public List<Country> getAll() {
 		return countryRepository.getAll();
 	}
